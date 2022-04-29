ebbd39997f2319212d61f3e4645f891f048aeb26
ALESSANDROUVA <a.uva1988@gmail.com>
2022-04-29 16:09:02 +0200

    error fatale

--- index.php
+++ index.php
@@ -1,4 +1,49 @@
 <?php
 
+class Movie
+{
+    private $title;
+    private $cast;
+    private $poster;
+    private $critica;
+    private $genere;
+    private $direttore;
+    private $scrittori;
+    private $dataUscita;
+    private $produzione;
+
+    public function __construct($_title, $_cast) {
+        $this->title = $_title;
+        $this->cast = $_cast;
+    }
+
+    public function setTitle($_title) { 
+        $this->title = $_title;
+        return $this;
+    }
+    
+    public function getTitle() { 
+        return $this->title;
+    }
+
+    public function setCast($_cast) { 
+        $this->cast = $_cast;
+        return $this;
+    }
+
+    public function getCast() { 
+        return $this->cast;
+    }
+
+
+
+
+
+}
+
+$Movie = new Movie('ciao', 'Vigo');
+
+echo $Movie
+
 
 ?>
